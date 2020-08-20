f668ba5031510b8079f548588f78960de25afed5
Kamal Nayan <mailtokamalnayan@gmail.com>
2020-08-20 16:13:25 +0200

    Init

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -3,42 +3,64 @@ import '../styles/ui.css';
 
 declare function require(path: string): any;
 
+async function getApiResponse(message) {
+    console.log('API Response for:', message);
+    try {
+        let response = await fetch(`https://reqres.in/api/users/2`);
+        console.log('API Data:', response);
+        return await response.json();
+    } catch (e) {
+        console.log(e);
+    }
+}
+
 const App = ({}) => {
     const textbox = React.useRef<HTMLInputElement>(undefined);
 
-    const countRef = React.useCallback((element: HTMLInputElement) => {
-        if (element) element.value = '5';
-        textbox.current = element;
-    }, []);
+    // const countRef = React.useCallback((element: HTMLInputElement) => {
+    //     if (element) element.value = '5';
+    //     textbox.current = element;
+    // }, []);
 
     const onCreate = React.useCallback(() => {
-        const count = parseInt(textbox.current.value, 10);
-        parent.postMessage({pluginMessage: {type: 'create-rectangles', count}}, '*');
+        // console.log('Hello world from App.tsx');
+        // const count = parseInt(textbox.current.value, 10);
+        parent.postMessage({pluginMessage: {type: 'spellcheck'}}, '*');
     }, []);
 
     const onCancel = React.useCallback(() => {
         parent.postMessage({pluginMessage: {type: 'cancel'}}, '*');
     }, []);
+    // const getApiResponse = React.useCallback(async message => {
+    //     console.log('Get API Response called');
+    //     let response = await fetch(`http://localhost:3000/?phrase=${message}`);
+    //     let data = await response.json();
+    //     console.log('API Data');
+
+    //     // parent.postMessage({pluginMessage: {type: 'checked', data}}, '*');
+    // }, []);
 
     React.useEffect(() => {
         // This is how we read messages sent from the plugin controller
-        window.onmessage = (event) => {
-            const { type, message } = event.data.pluginMessage;
-            if (type === 'create-rectangles') {
-                console.log(`Figma Says: ${message}`);
-            };
-        }
+        window.onmessage = event => {
+            const {type, message} = event.data.pluginMessage;
+            if (type === 'spellcheck') {
+                // var request = new XMLHttpRequest();
+                // request.open('GET', `http://localhost:3000/?phrase=${message}`);
+                // request.onload = () => {
+                //     console.log('From the response:', request.response);
+                //     // parent.postMessage({pluginMessage: request.response}, '*');
+                // };
+                // request.send();
+                getApiResponse(message[0]);
+            }
+        };
     }, []);
 
     return (
         <div>
-            <img src={require('../assets/logo.svg')} />
-            <h2>Rectangle Creator</h2>
-            <p>
-                Count: <input ref={countRef} />
-            </p>
             <button id="create" onClick={onCreate}>
-                Create
+                Spellcheck
             </button>
             <button onClick={onCancel}>Cancel</button>
         </div>
--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -1,25 +1,28 @@
 figma.showUI(__html__);
 
-figma.ui.onmessage = (msg) => {
-    if (msg.type === 'create-rectangles') {
-        const nodes = [];
+async function getPhrase() {
+    const wordNodes = figma.currentPage.findAll(node => node.type === 'TEXT');
+    const phrase = [];
+    for (let word of wordNodes) {
+        await figma.loadFontAsync(word.fontName);
+        // console.log(word.characters);
+        phrase.push(word.characters);
+    }
+    return phrase;
+}
 
-        for (let i = 0; i < msg.count; i++) {
-            const rect = figma.createRectangle();
-            rect.x = i * 150;
-            rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-            figma.currentPage.appendChild(rect);
-            nodes.push(rect);
-        }
-
-        figma.currentPage.selection = nodes;
-        figma.viewport.scrollAndZoomIntoView(nodes);
-
+figma.ui.onmessage = async msg => {
+    const phrase = await getPhrase();
+    if (msg.type === 'spellcheck') {
         // This is how figma responds back to the ui
         figma.ui.postMessage({
-            type: 'create-rectangles',
-            message: `Created ${msg.count} Rectangles`,
+            type: 'spellcheck',
+            message: phrase,
         });
+    }
+
+    if (msg.type === 'checked') {
+        console.log('Spell checked!');
     }
 
     figma.closePlugin();
